386198e83dd05cc23f544c17dcce7a61d446b428
Iaan van Niekerk <iaan@plusplusminus.co.za>
2015-07-29 17:23:51 +0200

    map styles and functions

--- wp-content/themes/campuskey/library/js/ppm.js
+++ wp-content/themes/campuskey/library/js/ppm.js
@@ -24,8 +24,10 @@ function initMap() {
 
 
 		map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 5,
-			center: {lat: -30.559482, lng: 22.937505999999985}
+			zoom: 6,
+			center: {lat: -29.559482, lng: 23.937505999999985},
+            scrollwheel: false,
+            styles:[{"featureType":"landscape","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"stylers":[{"hue":"#00aaff"},{"saturation":-100},{"gamma":2.15},{"lightness":12}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"visibility":"on"},{"lightness":24}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":57}]}]        
 		});
 
 		
