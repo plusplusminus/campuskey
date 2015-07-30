a107c39d09bda87b10cb0bd88d9c9d957b4d590e
Sergio Pellegrini <sergio@plusplusminus.co.za>
2015-07-30 10:02:39 +0200

    0.0.1

--- wp-content/themes/campuskey/sidebar.php
+++ wp-content/themes/campuskey/sidebar.php
@@ -3,8 +3,7 @@
   <?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
 
     <?php dynamic_sidebar( 'sidebar1' ); ?>
-  
-
+    
   <?php else : ?>
 
     <!-- This content shows up if there are no widgets defined in the backend. -->
