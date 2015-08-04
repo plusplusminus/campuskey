3b5f930b195459c64dd35ff990ea4da5a92e4eb1
Iaan van Niekerk <iaan@plusplusminus.co.za>
2015-08-04 16:13:06 +0200

    tweaks

--- wp-content/themes/campuskey/templates/lifestyle/features-content.php
+++ wp-content/themes/campuskey/templates/lifestyle/features-content.php
@@ -21,7 +21,11 @@ $connected = new WP_Query( array(
 
 			<?php while ( have_posts() ) : the_post(); $count++; ?>
 				<article class="lifestyle_content">
-					<?php the_content(); ?>
+					<div class="row">
+						<div class="col-md-offset-3 col-md-6 text-center">
+							<?php the_content(); ?>
+						</div>
+					</div>
 				</article>
 			<?php endwhile; ?>
 
@@ -40,24 +44,33 @@ $connected = new WP_Query( array(
 		) );
 
 		// Display connected pages
-		if ( $connected->have_posts() ) :
+	    if ( $connected->have_posts() ) : $count = 0;
 		?>
 		<aside class="section_feature"> 
 
 			<ul class="feature_list">
-				<?php while ( $connected->have_posts() ) : $connected->the_post(); ?>
-					<?php $iconname = get_post_meta($post->ID,'_ck_feature_icon',true); ?>
-					<li class="feature_list--item">
-						<div class="tab-ico"> 
-							<i class="icon-<?php echo $iconname; ?>"></i> 
-						</div>
-						<div class="tab-title">
-							<div class="display-table">
-								<div class="display-table-cell"><?php the_title(); ?></div>
-							</div>
-						</div>
-					</li>
-				<?php endwhile; ?>
+					<?php while ( $connected->have_posts() ) : $connected->the_post(); $count++;?>
+						<?php if ($count < 10) : ?>
+							<?php $iconname = get_post_meta($post->ID,'_ck_feature_icon',true); ?>
+		
+						    <li class="feature_list--item">
+								<div class="tab-ico"> 
+									<i class="icon-<?php echo $iconname; ?>"></i> 
+								</div>
+								<div class="tab-title">
+									<div class="display-table">
+										<div class="display-table-cell"><?php the_title(); ?></div>
+									</div>
+								</div>
+							</li>
+						<?php else: ?>
+							 <?php if ($count == 6) : ?>
+							 	<div class="clearfix"></div><br>
+							 	<li class="feature_list--list"><strong>Also included:</strong> </li>
+							 <?php endif; ?>
+							<li class="feature_list--list"><?php the_title(); ?></li>
+						<?php endif; ?>
+					<?php endwhile; ?>
 			</ul>
 
 		</aside>
