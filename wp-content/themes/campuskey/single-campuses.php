efa0b8939addddd9e9164ca4548521584c5b70f1
Sergio Pellegrini <sergio@plusplusminus.co.za>
2015-08-06 17:52:04 +0200

    responsive

--- wp-content/themes/campuskey/single-campuses.php
+++ wp-content/themes/campuskey/single-campuses.php
@@ -1,24 +1,18 @@
 <?php get_header(); ?>
 
-	<div class="choose-campus">
+   <div class="choose-campus">
+      <div class="inner">
+         <?php get_template_part('templates/campuses/header'); ?>
+         <?php get_template_part('templates/campuses/gallery-content'); ?>
+      </div>
+   	<div class="border-campus"></div>
+   </div>
 
-		<div class="inner">
-      
-	   		<?php get_template_part('templates/campuses/header'); ?>
+   <?php get_template_part('templates/campuses/rooms'); ?>
 
-	   		<?php get_template_part('templates/campuses/gallery-content'); ?>
+   <?php get_template_part('templates/campuses/buildings'); ?>
 
-   		</div>
-
-   		<div class="border-campus"></div>
-
-   	</div>
-
-   	<?php get_template_part('templates/campuses/rooms'); ?>
-
-   	<?php get_template_part('templates/campuses/buildings'); ?>
-
-   	<?php get_template_part('templates/section','news'); ?>
+   <?php get_template_part('templates/section','news'); ?>
 
 
 <?php get_footer(); ?>
