96c718e88279c632f0c1f40b9ba60b50129d6729
Iaan van Niekerk <iaan@plusplusminus.co.za>
2015-08-16 13:50:33 +0200

    minute detail tweaks

--- wp-content/themes/campuskey/library/js/ppm.js
+++ wp-content/themes/campuskey/library/js/ppm.js
@@ -1,3 +1,14 @@
+jQuery.fn.highlight = function (str, className) {    
+    var regex = new RegExp(str, "gi");
+
+    return this.each(function () {
+        this.innerHTML = this.innerHTML.replace(regex, function(matched) {return "<span class=\"" + className + "\">" + matched + "</span>";});
+    });
+};
+
+jQuery("li a").highlight("yl","letterspace");
+
+
 jQuery(window).load(function() {
 	var $container = jQuery('.featured_gallery');
     // init
