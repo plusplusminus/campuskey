397b03315bb801950b2f7c8fc8074e1b5188cde6
Iaan van Niekerk <iaan@plusplusminus.co.za>
2015-08-19 12:36:01 +0200

    floorplan captions

--- wp-content/themes/campuskey/single-rooms.php
+++ wp-content/themes/campuskey/single-rooms.php
@@ -44,7 +44,7 @@
 					  					<?php if($key) : ?>
 
 					  						<?php $image_attributes_large = wp_get_attachment_image_src( $plan['name_id'],'full' ); ?>
-					  						<a class="fancybox" rel="group1" href="<?php echo $image_attributes_large[0];?>">
+					  						<a class="fancybox" data-fancybox-title="<?php echo wpautop($plan['caption']); ?>" rel="group1" href="<?php echo $image_attributes_large[0];?>">
 												<li class="slide" style="background-image:url('<?php echo $image_attributes_large[0];?>');">
 													<div class="img_caption"><?php echo wpautop($plan['caption']); ?></div>
 												</li>
@@ -57,7 +57,7 @@
 					  					<?php $image_attributes_large = wp_get_attachment_image_src( $plan['name_id'],'full' ); ?>
 					  						
 												<li class="slide" style="background-image:url('<?php echo $image_attributes_large[0];?>');">
-													<a class="fancybox" rel="group1" href="<?php echo $image_attributes_large[0];?>">
+													<a class="fancybox" data-fancybox-title="<?php echo wpautop($plan['caption']); ?>" rel="group1" href="<?php echo $image_attributes_large[0];?>">
 														<div class="img_caption"><?php echo wpautop($plan['caption']); ?></div>
 													</a>
 												</li>
